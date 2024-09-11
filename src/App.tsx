4f30bba55d45c7ffe23cdbcef8197050b449c88b
Enriquedonaire <donaire.q2@gmail.com>
2024-09-11 13:07:00 +0200

    Add Basename

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './views/LoginPage/LoginPage';
 import UsersPage from './views/UsersPage/UsersPage';
-import NotFoundPage from './views/NotFoundPage/NotFoundPage';
+import NotFoundPage from './views//NotFoundPage/NotFoundPage';
 import PrivateRoute from './components/PrivateRoute';
 import { ThemeContextProvider } from './context/Themes';
 
 const App: React.FC = () => {
   return (
-    <ThemeContextProvider> 
-      <Router>
+    <ThemeContextProvider>
+      <Router basename="/Alea-Test">
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/login" element={<LoginPage />} />
